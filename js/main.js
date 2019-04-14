523fffbab674e82f104b97651e3c3b9d577809a7
iversonLv <iverson_3_3_3@126.com>
2019-04-15 01:22:34 +0800

    issue_39 revert all caps to previous text state

--- js/main.js
+++ js/main.js
@@ -431,9 +431,9 @@ document.addEventListener("DOMContentLoaded", function() {
       forceClickToSublevel1(switchItem)
     }
 
-    if (switchText === 'SWITCH TO ' + bussinessItemVlue) {
+    if (switchText === 'Switch to ' + bussinessItemVlue) {
       swtichValue(workItemVlue, bussinessItem)
-    } else if (switchText === 'SWITCH TO ' + workItemVlue) {
+    } else if (switchText === 'Switch to ' + workItemVlue) {
       swtichValue(bussinessItemVlue, workItem)
     }
 
@@ -457,11 +457,11 @@ document.addEventListener("DOMContentLoaded", function() {
       return;
     }
 
-    // After change the navigation, the switch to text should toggle as well
+    // After change the navigation, the Switch to text should toggle as well
     if (target.innerHTML === 'BUSINESS') {
-      document.getElementsByClassName('switch-to-busniness')[0].innerHTML = 'SWITCH TO WORK'
+      document.getElementsByClassName('switch-to-busniness')[0].innerHTML = 'Switch to WORK'
     } else if (target.innerHTML === 'WORK') {
-      document.getElementsByClassName('switch-to-busniness')[0].innerHTML = 'SWITCH TO BUSINESS'
+      document.getElementsByClassName('switch-to-busniness')[0].innerHTML = 'Switch to BUSINESS'
     }
   
     polulateSecondaryNavMobile(target);
